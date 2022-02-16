01d6253ce5f45f408dbf318d29bd56c8609f13d5
camila15 <mcamilas1599@gmail.com>
2022-02-16 13:46:26 -0500

    first commit

---
+++ showtimes/src/test/java/co/edu/poli/showtimes/ShowtimesApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.poli.showtimes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShowtimesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
